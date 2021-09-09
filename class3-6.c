a34f04fcedec7d783d274bcd758dfd3229d7beac
誉杰 蹇 <1870121840@qq.com>
2021-09-09 22:08:16 +0800

    无说明

---
+++ class3-6.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+/*
+int main() {
+	char str[] = "China";
+	for (int i = 0; i < 5; i++) {
+		str[i] = str[i] + 4;
+	}
+	for (int i = 0; i < 5; i++) {
+		printf("%c", str[i]);
+	}
+	
+}
+*/
---
+++ class4-4.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+//3个整数，输出最大的数
+/*
+int main() {
+	int a, b, c,t;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a < b) {
+		t = a; a = b; b = t;
+	}
+	if (a < c) {
+	t = a; a = c; c = t;
+	printf("%d", a);
+	}
+	else 
+		printf("%d", a);
+}
+*/

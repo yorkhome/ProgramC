77f2f427fd66ef794fed0b84e37ccb0f3c0f002c
誉杰 蹇 <1870121840@qq.com>
2021-09-27 23:00:52 +0800

    class5 end

---
+++ class5-12.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//
+/*
+int main() {
+	int x=1;
+	for (int i = 1; i < 10; i++) {
+		x = (x + 1) * 2;
+	}
+	printf("%d", x);
+}
+*/
---
+++ class5-16.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//
+/*
+int main() {
+	int j, k;
+	for (int i = 4; i >= 1; i--) {
+		for (j = i; j > 1; j--)
+			printf(" ");
+		for (k = 0; k < 2 * (5-i) - 1; k++)
+			printf("*");
+		printf("\n");
+	}
+	for (int i = 3; i >= 1; i--) {
+		for (j = 4-i; j >=1; j--)
+			printf(" ");
+		for (k = 2 * i - 1; k >0; k--)
+			printf("*");
+		printf("\n");
+	}
+}
+*/

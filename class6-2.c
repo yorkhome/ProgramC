13ec8b0ef59ce7fd0ea462f39c4d82818394cbd0
誉杰 蹇 <1870121840@qq.com>
2021-10-04 23:32:09 +0800

    选择排序

---
+++ class6-2.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//—°‘Ò≈≈–Ú
+/*
+int main() {
+	int t,min,a[] = {9,8,7,6,5,4,3,2,1,0};
+	for (int i = 0; i < 9; i++) {
+		min = i;
+		for (int j = i+1; j < 10; j++)
+			if (a[min] > a[j]) min = j;
+		t = a[i];
+		a[i] = a[min];
+		a[min] = t;
+	}
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", a[i]);
+	}
+}
+*/

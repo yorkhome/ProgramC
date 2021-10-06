f4580bf27f1246cf44e15aa07c9e6608d90ddb5e
誉杰 蹇 <1870121840@qq.com>
2021-10-06 21:49:44 +0800

    逆序

---
+++ class6-5.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//ÄæÐò
+/*
+int main() {
+	int a[5] = { 8,6,5,4,1 }, b[5];
+	for (int i = 0; i < 5; i++) {
+		b[i] = a[4 - i];
+	}
+	for(int i=0;i<5;i++)
+		printf("%d ", b[i]);
+}
+*/

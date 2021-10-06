8334b2abcfcbaa53528d909f2d01947dd60abc72
誉杰 蹇 <1870121840@qq.com>
2021-10-06 20:47:08 +0800

    对角线和

---
+++ class6-3.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//对角线和
+/*
+int main() {
+	int a[3][3], sum=0;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			scanf("%d", &a[i][j]);
+		}
+	}
+	for (int i = 0; i < 3; i++)
+		sum = sum + a[i][i];
+	printf("对角线和为：%d", sum);
+}
+*/

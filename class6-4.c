7d65f686bb10cabe4847fb9c555ea1eb0a2d6536
誉杰 蹇 <1870121840@qq.com>
2021-10-06 21:25:32 +0800

    插入数字

---
+++ class6-4.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//²åÈëÊý×Ö
+/*
+int main() {
+	int n,temp1,temp2,a[11] = { 1,4,6,8,9,12,14,16,17,23 };
+	scanf("%d", &n);
+	for (int i = 0; i < 10; i++) {
+		if (a[i] > n) {
+			temp1 = a[i];
+			a[i] = n;
+			for (int j = i + 1; j < 11; j++) {
+				temp2 = a[j];
+				a[j] = temp1;
+				temp1 = temp2;
+			}
+			break;
+		}
+	}
+	for(int i=0;i<11;i++)
+		printf("%d ", a[i]);
+}
+*/
